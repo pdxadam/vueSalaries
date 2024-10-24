d70ce2bd625dafecb6cce6e82c88db84c441d2ac
pdxadam <pdxadam@gmail.com>
2024-10-24 09:38:20 -0700

    going back to 2D Array

---
+++ Salaries/src/classes/Cell.js
@@ -0,0 +1,8 @@
+export default class Cell{
+    salary = 0.0;
+    fte = 0.0;
+    constructor(salary){
+        this.salary = salary;
+        
+    }
+}
--- Salaries/src/classes/Schedule.js
+++ Salaries/src/classes/Schedule.js
@@ -1,25 +1,33 @@
 export default class Schedule{
     title = "New Schedule";
-    salary = [[]];
+    rows = 0;
+    cols = 0;
+    colTitles = [];
+    rowTitles = [];
+    cells = [];
+
     constructor(title){
         this.title = title;
     }
     static SeedSchedule(title, rows, columns){
+        this.rows = rows;
+        this.cols = cols;
         var s = new Schedule(title);
-        for (var row = 0; row <= rows; row++){
+        for (var row = 0; row < rows; row++){
             //an extra is being added because of titles
+            rowTitles.push("Row " + row);
+            cells.push([]);
             for (var col = 0; col <= columns; col++){
-                if (col == 0 && row==0){
-                    s.salary[col][row] = "Schedule";
-                }
-                else if (row == 0){
-                    s.salary[col][row] = "Column";
-                }
-                else if (col == 0){
-                    s.salary[col][row] = "25.15";
-                }
+                    if (row == 0){ //only pop titles one time through
+                        colTitles.push("Column " + col);
+                    }
+                
+                    s.cells[col].push(new Cell(25.25));
+                    s.cells[col][row].fte = 1.0;
+
             }
         }
+        print(s);
         return s;
     }
 
