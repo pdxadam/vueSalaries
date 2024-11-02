02485795bc2213132b4d1163cff5e584a03db207
Adam McLain <pdxadam@gmail.com>
2024-11-02 07:41:15 -0700

    Update About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This app is designed to help education bargaining teams evaluate the financial impacts of proposed contracts. You can check out or contribute to the project on the <a href="https://github.com/pdxadam/vueSalaries?tab=readme-ov-file#readme" target="_blank"> project's Github</a></p>
+    <p>You should absolutely download backups of your Scenarios. The data is all saved on your computer in your browser, so the only way to share the info is to download scenarios and send those files around.</p>
   </div>
 </template>
 
-<style>
+<style scoped>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+  }
+  h1{
+    font-size: 1.5em;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+  p{
+    background-color: aliceblue;
+    margin: 15px;
+    padding: 15px;
   }
 }
 </style>
